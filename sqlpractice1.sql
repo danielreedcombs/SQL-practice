cd1f6aa5113f0a2c40d79813bb3fb09bd3c78290
Daniel Combs <danielreedcombs@gmail.com>
2019-05-06 08:49:22 -0500

    2 more chapters complete

--- sqlpractice1.sql
+++ sqlpractice1.sql
@@ -55,3 +55,30 @@ LIMIT 5 OFFSET 5;
 
 -- chapter 5
 
+SELECT title, domestic_sales, international_sales
+FROM movies
+  JOIN boxoffice
+    ON movies.id = boxoffice.movie_id;
+
+SELECT title, domestic_sales, international_sales
+FROM movies
+  JOIN boxoffice
+    ON movies.id = boxoffice.movie_id
+WHERE international_sales > domestic_sales;
+
+SELECT title, rating
+FROM movies
+  JOIN boxoffice
+    ON movies.id = boxoffice.movie_id
+ORDER BY rating DESC;
+
+-- chapter 6
+
+SELECT DISTINCT building FROM employees;
+
+SELECT * FROM buildings;
+
+SELECT DISTINCT building_name, role
+FROM buildings
+  LEFT JOIN employees
+    ON building_name = building;
