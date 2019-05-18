fde4ec0bbad8ed48760cb19f5627423e7b974ba4
Daniel Combs <danielreedcombs@gmail.com>
2019-05-18 11:47:08 -0500

    sql chapter 7

--- sqlpractice1.sql
+++ sqlpractice1.sql
@@ -82,3 +82,14 @@ SELECT DISTINCT building_name, role
 FROM buildings
   LEFT JOIN employees
     ON building_name = building;
+
+-- chapter 7
+
+SELECT DISTINCT building FROM employees;
+
+SELECT * FROM buildings;
+
+SELECT DISTINCT building_name, role
+FROM buildings
+  LEFT JOIN employees
+    ON building_name = building;
